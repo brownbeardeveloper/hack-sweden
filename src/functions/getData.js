4c4c484d31bb15da68f07fa124fc5762ec499b54
Franz - Josef Björck <fj.bjorck@protonmail.com>
2024-04-27 15:05:14 +0200

    refactor: abstracted the url

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -1,5 +1,4 @@
 export default async function getData(url) {
-
-    return await fetch("https://skatteverket.entryscape.net/rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99?kommun=MALM%C3%96&_limit=100&_offset=0").then(resp => resp.json())
+    return await fetch(process.env.REACT_APP_SKATTESATS_API + "?kommun=MALM%C3%96&_limit=100&_offset=0").then(resp => resp.json())
   }
   
