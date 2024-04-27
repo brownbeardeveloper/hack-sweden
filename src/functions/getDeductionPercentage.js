c69644b0189735ef1f825176ae913f64e99ae0dd
Franz - Josef Björck <fj.bjorck@protonmail.com>
2024-04-27 16:34:52 +0200

    feat: added deduction percentage getting

---
+++ src/functions/getDeductionPercentage.js
@@ -0,0 +1,15 @@
+export default async function getDeductionPercentage({table, year, income}){
+    const url = process.env.REACT_APP_SKATTEAVDRAG_API
+    const params = new URLSearchParams()
+    params.append("år", year)
+    params.append("tabell", table)
+    params.append("_limit","100")
+    params.append("_offset","0")
+    const resp = await fetch(url + params).then(resp => resp.json())
+    return resp.results.filter(e => e["ink from"] < income && e["ink tom"] > income).map(e => e["%"])
+
+
+
+
+
+}
