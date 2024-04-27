e2fbd5409e3959f0572111dad247f552adacb138
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-04-27 15:32:20 +0200

    Created data for Malmö's budget allocation

---
+++ src/data/malmo-budget-allocation.js
@@ -0,0 +1,10 @@
+const budgetAllocation = {
+    preschoolPrimarySecondaryAndAdultEducation: 0.46,
+    elderlyCareAndCareForPersonsWithDisabilities: 0.26,
+    individualAndFamilyCare: 0.15,
+    cultureAndRecreationalActivities: 0.06,
+    infrastructureAndProtection: 0.05,
+    otherActivities: 0.02
+};
+
+export default budgetAllocation;
