05265f30dcb05f909bb5303f19c94917098ab667
Franz - Josef Björck <fj.bjorck@protonmail.com>
2024-04-27 17:15:59 +0200

    feat: added grossSalary, year and city as states for easier handling

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
+import getTaxBracket from "../functions/getTaxBracket";
+import getDeductionPercentage from "../functions/getDeductionPercentage";
 
 export default function FormComponent() {
     const [netSalary, setNetSalary] = useState('');
-    const [percentageTax, setPercentageTax] = useState('');
+    const [grossSalary, setGrossSalary] = useState(0);
+    const [taxPercentage, setTaxPercentage] = useState(0);
+    const [year, setYear] = useState(2023);
+    const [city, setCity] = useState('MALMÖ');
 
     function handleSubmit(e) {
         e.preventDefault();
         console.log("Nettolön:", netSalary);
-        console.log("Kommunalskatt i procent:", percentageTax);
+        console.log("Kommunalskatt i procent:", taxPercentage);
     }
 
     return (
@@ -25,6 +30,26 @@ export default function FormComponent() {
 
                 </div>
                 <div className='m-2'>
+                    <label htmlFor="grossSalaryInput">Bruttolön</label>
+                    <input
+                        type="number"
+                        id="grossSalaryInput"
+                        min="0"
+                        value={grossSalary}
+                        onChange={async (e) => {
+                            setGrossSalary(e.target.value)
+                            setTaxPercentage(
+                                await getTaxBracket({kommun: city, year: year}))
+                            console.log(await getDeductionPercentage({
+                                table: Math.floor(taxPercentage),
+                                year: year,
+                                income: grossSalary
+                            }))
+                        }}
+                    />
+
+                </div>
+                <div className='m-2'>
                     <label htmlFor="percentageTaxInput">Kommunalskatt i procent </label>
                     <input
                         type="number"
@@ -33,13 +58,14 @@ export default function FormComponent() {
                         max="36"
                         step="0.01"
                         placeholder="30.00%"
-                        value={percentageTax}
-                        onChange={(e) => setPercentageTax(e.target.value)}
+                        value={taxPercentage}
+                        onChange={(e) => setTaxPercentage(e.target.value)}
                     />
 
                 </div>
                 <button type="submit" className='button'>Submit</button>
             </form>
+            <h1>{taxPercentage}</h1>
         </div>
     );
 }
