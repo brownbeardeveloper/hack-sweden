a15f1e8f3dd96789054a29bba845a97ecb4e533a
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-04-28 13:42:06 +0200

    Adjusted the style

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -45,78 +45,80 @@ export default function FormComponent() {
 
 
     return (
-        <div className='flex flex-row justify-center items-center'>
-            <form
-                className='flex flex-col justify-center items-start bg-linear-gradient rounded-lg p-8 border border-slate-400'>
+        <div className=''>
+            <div className='flex flex-row justify-center items-center'>
+                <form
+                    className='flex flex-col justify-center items-start bg-linear-gradient rounded-lg p-8 border border-slate-400'>
 
-                <div className='flex flex-row mr-2 my-1'>
-                    <label className="mx-1" htmlFor={"medlemITrossamfund"}>Medlem i Trossamfund?</label>
-                    <input className="mx-1" id={"medlemITrossamfund"} type={"checkbox"} value={kyrkoAvgift}
-                        onChange={event => setKyrkoAvgift(event.target.checked)} />
-                </div>
-
-                <div className='flex flex-row mr-2 my-1'>
-                    <label className="mx-1" htmlFor={"stad"}>Stad</label>
-                    <select className="mx-1" onChange={event => handleCityChange(event)} id={"stad"}>
-                        <option value={"MALMÖ"} selected={true}>Malmö</option>
-                        <option value={"LANDSKRONA"}>Landskrona</option>
-                    </select>
-                </div>
-
-
-                {kyrkoAvgift &&
                     <div className='flex flex-row mr-2 my-1'>
-                        <label className="mx-1" htmlFor={"stad"}>Trossamfund
-                            <select id={"trossamfund"}
-                                onChange={event => handleReligiousPlaceChange(event)}>
-                                <ReligiousList places={religiousPlaces} />
-                            </select>
-                        </label>
+                        <label className="mx-1" htmlFor={"medlemITrossamfund"}>Medlem i Trossamfund?</label>
+                        <input className="mx-1" id={"medlemITrossamfund"} type={"checkbox"} value={kyrkoAvgift}
+                            onChange={event => setKyrkoAvgift(event.target.checked)} />
+                    </div>
+
+                    <div className='flex flex-row mr-2 my-1'>
+                        <label className="mx-1" htmlFor={"stad"}>Stad</label>
+                        <select className="mx-1" onChange={event => handleCityChange(event)} id={"stad"}>
+                            <option value={"MALMÖ"} selected={true}>Malmö</option>
+                            <option value={"LANDSKRONA"}>Landskrona</option>
+                        </select>
+                    </div>
+
+
+                    {kyrkoAvgift &&
+                        <div className='flex flex-row mr-2 my-1'>
+                            <label className="mx-1" htmlFor={"stad"}>Trossamfund
+                                <select id={"trossamfund"}
+                                    onChange={event => handleReligiousPlaceChange(event)}>
+                                    <ReligiousList places={religiousPlaces} />
+                                </select>
+                            </label>
+                        </div>
+                    }
+
+                    <div className='flex flex-row mr-2 my-1'>
+                        <label className="mx-1" htmlFor={"year"}>År:</label>
+                        <input className="mx-1" value={year} id={"year"} type="number" onChange={event => setYear(event.target.value)} min="2000" max="2023"
+                            step="1" />
+                    </div>
+
+
+                    <div className='flex flex-row mr-2 my-1'>
+                        <label className='mx-1' htmlFor="grossSalaryInput">Bruttolön</label>
+                        <input
+                            type="number"
+                            id="grossSalaryInput"
+                            min="0"
+                            value={grossSalary}
+                            onChange={event => handleOnGrossPayChange(event)}
+                        />
+                    </div>
+                </form>
+            </div>
+
+            <div className='m-10'>
+
+                {taxBracket &&
+                    <div className="bg-gray-50 p-8 rounded-lg shadow-md">
+                        <h1 className="text-xl font-bold mb-4">{taxDeduction} reduction each month</h1>
+                        <p>skattetabell: {Math.round(kyrkoAvgift ? taxBracket["summa, inkl. kyrkoavgift"] : taxBracket["summa, exkl. kyrkoavgift"])}</p>
+                        <p>Kommunal skatt: {Math.round(taxDeduction * (taxBracket["kommunal-skatt"] / 100))}</p>
+                        <ul className="list-disc pl-4 mb-4">
+                            <li>Förskola, grundskola och gymnasieskola: {communalTax.education}</li>
+                            <li>Äldreomsorg och funktionsstöd: {communalTax.elderCare}</li>
+                            <li>Individ- och familjeomsorg: {communalTax.socialCare}</li>
+                            <li>Kultur- och fritidsverksamhet: {communalTax.culture}</li>
+                            <li>Gata, park och räddningstjänst: {communalTax.infrastructureAndFirstResponse}</li>
+                            <li>Övrig verksamhet: {communalTax.other}</li>
+                        </ul>
+                        <p>Total: {communalTax.education + communalTax.elderCare + communalTax.socialCare + communalTax.culture + communalTax.infrastructureAndFirstResponse + communalTax.other}</p>
+                        <p>Landstingsskatt: {Math.round(taxDeduction * (taxBracket["landstings-skatt"] / 100))}</p>
+                        <p>Begravnings avgift: {Math.round(taxDeduction * (taxBracket["begravnings-avgift"] / 100))}</p>
+                        <p>Kyrkoavgift: {Math.round(kyrkoAvgift ? taxDeduction * (taxBracket["kyrkoavgift"] / 100) : 0)}</p>
+                        <p>Nettolön: {netSalary}</p>
                     </div>
                 }
-
-                <div className='flex flex-row mr-2 my-1'>
-                    <label className="mx-1" htmlFor={"year"}>År:</label>
-                    <input className="mx-1" value={year} id={"year"} type="number" onChange={event => setYear(event.target.value)} min="2000" max="2023"
-                        step="1" />
-                </div>
-
-
-                <div className='flex flex-row mr-2 my-1'>
-                    <label className='mx-1' htmlFor="grossSalaryInput">Bruttolön</label>
-                    <input
-                        type="number"
-                        id="grossSalaryInput"
-                        min="0"
-                        value={grossSalary}
-                        onChange={event => handleOnGrossPayChange(event)}
-                    />
-                </div>
-
-
-
-            </form>
-
-            {taxBracket &&
-                <div className="bg-gray-100 p-8 rounded-lg shadow-md">
-                    <h1 className="text-xl font-bold mb-4">{taxDeduction} reduction each month</h1>
-                    <p>skattetabell: {Math.round(kyrkoAvgift ? taxBracket["summa, inkl. kyrkoavgift"] : taxBracket["summa, exkl. kyrkoavgift"])}</p>
-                    <p>Kommunal skatt: {Math.round(taxDeduction * (taxBracket["kommunal-skatt"] / 100))}</p>
-                    <ul className="list-disc pl-4 mb-4">
-                        <li>Förskola, grundskola och gymnasieskola: {communalTax.education}</li>
-                        <li>Äldreomsorg och funktionsstöd: {communalTax.elderCare}</li>
-                        <li>Individ- och familjeomsorg: {communalTax.socialCare}</li>
-                        <li>Kultur- och fritidsverksamhet: {communalTax.culture}</li>
-                        <li>Gata, park och räddningstjänst: {communalTax.infrastructureAndFirstResponse}</li>
-                        <li>Övrig verksamhet: {communalTax.other}</li>
-                    </ul>
-                    <p>Total: {communalTax.education + communalTax.elderCare + communalTax.socialCare + communalTax.culture + communalTax.infrastructureAndFirstResponse + communalTax.other}</p>
-                    <p>Landstingsskatt: {Math.round(taxDeduction * (taxBracket["landstings-skatt"] / 100))}</p>
-                    <p>Begravnings avgift: {Math.round(taxDeduction * (taxBracket["begravnings-avgift"] / 100))}</p>
-                    <p>Kyrkoavgift: {Math.round(kyrkoAvgift ? taxDeduction * (taxBracket["kyrkoavgift"] / 100) : 0)}</p>
-                    <p>Nettolön: {netSalary}</p>
-                </div>
-            }
+            </div>
         </div>
     );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,7 +25,6 @@ export default function Navbar() {
                         </button>
                     </li>
 
-
                     {sidebarData.map((item, index) => (
                         <a
                             key={index}
